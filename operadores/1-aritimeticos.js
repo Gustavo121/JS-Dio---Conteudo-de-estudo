67bf304ca056b7c6cc9cac80d57ebd62eb523502
Gustavo <gustgouvea@hotmail.com>
2022-03-28 11:10:06 -0300

    estudos operadores dio

---
+++ operadores/1-aritimeticos.js
@@ -0,0 +1,29 @@
+// Módulo (%)
+// Operador binário. Retorna o inteiro restante da divisão dos dois operandos.
+
+12 % 5 // retorna 2.
+
+// Incremento (++) e Decremento (--)   Operador Unario
+++x
+x++
+
+const a = ++2; // 3     incrimenta na hora
+const b = 2++; // 2     retorna o valor depois incrementa
+
+--x 
+x--
+
+// Negação (-) e Adição (+)
+// + força o valor de uma string para numerico.
+-3
++"3"        // retorna 3
++true       // retorna 1
++false      // retorna 0
+-true       // retorna -1
+
+// Operador de exponenciação (**)
+2 ** 3      // retorna 8
+10 ** -1    // retorna 0.1
+
+// Operador de agrupamento ()
+2 * (3 + 2)
---
+++ operadores/1-condicional.js
@@ -0,0 +1,7 @@
+// Ternário
+condicao ? valor1 : valor2
+
+true ? 'foo' : 'bar'        // Retorna 'foo'
+false ? 'foo' : 'bar'       // Retorna 'bar'
+
+// se a condição for verdadeira ela recebe o primeiro valor, se não recebe o segundo.
